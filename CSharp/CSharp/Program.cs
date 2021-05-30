6b511e3ed8a234b855468c808471900a2d9b1572
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-05-30 18:04:56 -0400

    Memory allocations project was added

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -7,16 +7,31 @@ namespace CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var experiments = new Experiments("Maksim");
             Experiments.Reflection();
+            experiments.Name = "Anya";
         }
     }
 
     public class Experiments
     {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public Experiments(string name)
+        {
+            _name = name;
+        }
+
         public static void Reflection()
         {
             var reflection = typeof(Experiments);
-            Console.WriteLine();
+            var reflectionMemberTypes = reflection.MemberType;
         }
     }
 }
---
+++ CSharp/MemoryAllocations/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace MemoryAllocations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var application = new Application();
+            application.PrintDateFromLocalVariables();
+            application.PrintDateFromLocalStruct();
+            application.PrintDateFromClass();
+        }
+    }
+
+    public class Application
+    {
+        public void PrintDateFromLocalVariables()
+        {
+            int day = 18;
+            int month = 12;
+            int year = 2021;
+            
+            unsafe
+            {
+                int* pointer = &year;
+                year++;
+            }
+
+            Console.WriteLine($"The date is: {day}/{month}/{year}");
+        }
+        
+        public void PrintDateFromLocalStruct()
+        {
+            DateStruct dateStruct = new DateStruct
+            {
+                Day = 18,
+                Month = 12,
+                Year = 2021
+            };
+
+            Console.WriteLine($"The date is: {dateStruct.Day}/{dateStruct.Month}/{dateStruct.Year}");
+        }
+        
+        public void PrintDateFromClass()
+        {
+            DateClass dateClass = new DateClass
+            {
+                Day = 18,
+                Month = 12,
+                Year = 2021
+            };
+
+            Console.WriteLine($"The date is: {dateClass.Day}/{dateClass.Month}/{dateClass.Year}");
+        }
+    }
+    
+    public ref struct DateStruct
+    {
+        public int Day { get; init; }
+
+        public int Month { get; init; }
+
+        public int Year { get; init; }
+    }
+    
+    public class DateClass
+    {
+        public int Day { get; init; }
+
+        public int Month { get; init; }
+
+        public int Year { get; init; }
+    }
+}
